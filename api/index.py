11ab5b2f927f62e9c203f45d1daa1ccc05597938
Jdss026 <jdss026@gmail.com>
2023-06-24 15:32:30 -0300

    changes

--- api/index.py
+++ api/index.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'Hello, World! 2'
 
 @app.route('/about')
 def about():
-    return 'About'
+    return 'About this page'
